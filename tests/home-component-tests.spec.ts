47eaf73f4b0a392e32e01d9902165a33b91ffbd0
TiagoGouveia-BSimple <tiago.gouveia@b-simple.pt>
2025-08-04 16:44:07 +0100

    experiments with playwright

--- tests/home-component-tests.spec.ts
+++ tests/home-component-tests.spec.ts
@@ -1,26 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('should show login componenent', async ({page}) => {
+test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200');
+});
+
+test('should show login componenent', async ({page}) => {
     await expect(page.locator('app-login')).toBeVisible();
 });
 
 test('should show register component', async({page})=> {
-    await page.goto('http://localhost:4200');
-
     await page.getByTestId('register-btn').click();
     await expect(page.locator('app-register')).toBeVisible();
 });
 
 test('should register user, show and alert and then return to login page', async({page}) => {
-    await page.goto('http://localhost:4200');
-
     // go to register page
     await page.getByTestId('register-btn').click();
     await expect(page.locator('app-register')).toBeVisible();
 
     // fill the form
-    await page.locator('input#name').fill('João');
     await page.locator('input#name').fill('João');
     await page.locator('input#surname').fill('Silva');
     await page.locator('input#email').fill('joao@email.com');
@@ -39,15 +37,30 @@ test('should register user, show and alert and then return to login page', async
     await expect(page.locator('app-login')).toBeVisible();
 });
 
-test('should register user, login, show user info then logout and show login component', async({page})=> {
-    await page.goto('http://localhost:4200');
+test('should show allert and return to login page when logging in with non existent user', async({page}) => {
+    // fill the form without creating user
+    await page.locator('input#email').fill('joao@email.com');
+    await page.locator('input#password').fill('123456');
 
+    // submit form
+    await page.locator('button[type="submit"]').click();
+
+    // check if alert is correct
+    page.once('dialog', async (dialog) => {
+        expect(dialog.message()).toBe('Invalid login data..');
+        await dialog.accept();
+    });
+
+    // show login component again
+    await expect(page.locator('app-login')).toBeVisible();
+});
+
+test('should register user, login, show user info then logout and show login component', async({page})=> {
     // go to register page
     await page.getByTestId('register-btn').click();
     await expect(page.locator('app-register')).toBeVisible();
 
     // fill the form
-    await page.locator('input#name').fill('João');
     await page.locator('input#name').fill('João');
     await page.locator('input#surname').fill('Silva');
     await page.locator('input#email').fill('joao@email.com');
@@ -79,4 +92,46 @@ test('should register user, login, show user info then logout and show login com
     // should click logout button and show login component
     await page.locator('button').click();
     await expect(page.locator('app-login')).toBeVisible();
+});
+
+test('should show alert and return to login page when logging in with non existent user', async({page}) => {
+    // fill the form without creating user
+    await page.locator('input#email').fill('joao@email.com');
+    await page.locator('input#password').fill('123456');
+
+    // submit form
+    await page.locator('button[type="submit"]').click();
+
+    // check if alert is correct
+    page.once('dialog', async (dialog) => {
+        expect(dialog.message()).toBe('Invalid login data..');
+        await dialog.accept();
+    });
+
+    // show login component again
+    await expect(page.locator('app-login')).toBeVisible();
+});
+
+test('should not register user and show alert when registerig with invalid data', async({page})=> {
+    // go to register page
+    await page.getByTestId('register-btn').click();
+    await expect(page.locator('app-register')).toBeVisible();
+
+    // fill the form
+    await page.locator('input#name').fill('João');
+    await page.locator('input#surname').fill('Silva');
+    await page.locator('input#email').fill('joao');
+    await page.locator('input#password').fill('123456');
+
+    // submit form
+    await page.locator('button[type="submit"]').click();
+
+    // check if alert is correct
+    page.once('dialog', async (dialog) => {
+        expect(dialog.message()).toBe('Invalid user data');
+        await dialog.accept();
+    });
+
+    // show register component again
+    await expect(page.locator('app-register')).toBeVisible();
 });
